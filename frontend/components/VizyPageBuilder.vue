b7752d1fc2602ee22066bc2bbd4ba198d9218d6a
Taylor <taylorcgaines@gmail.com>
2022-01-30 20:45:33 -0700

    gql for vizy set up, backup added for a db with lorem

---
+++ frontend/components/VizyPageBuilder.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot />
+    <!-- BLOCK -->
+    <!-- TWO COLUMN -->
+    <!-- THREE COLUMN -->
+    <!-- COLUMN -->
+    <!-- COPY -->
+    <!-- IMAGE -->
+    <!-- FREEFORM -->
+    <!-- FORMIE -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VizyPageBuilder',
+}
+</script>
